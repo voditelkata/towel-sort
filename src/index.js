4632f3eeb553d5492d3ccbc8581ce18241ee0e18
voditelkatka <voditelkatka@gmail.com>
2021-03-04 19:18:40 +0300

    add: addition reversed elements of array

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    let b = [];
-for (let i = 0; i < matrix.length; i++) {
-    for (j = 0; j < matrix[i].length; j++) {
-        b.push(matrix[i][j]);
+    let emptyArray = [];
+    if (matrix == undefined){
+        return emptyArray;
     }
-}
-  return b;
+    let b = [];
+   for (let i = 0; i < matrix.length; i++) {
+         if (i % 2 == 0){
+         b.push(matrix[i]);
+        } else {
+         b.push(matrix[i].reverse());
+        }  
+ }
+let result = [];
+ for (q = 0; q < b.length; q++){
+     for (w = 0; w < b[q].length; w++){
+         result.push(b[q][w]);
+     }
+ }
+   return result;
 }
