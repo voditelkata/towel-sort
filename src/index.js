8bc4cd665180eca18c6a39d04bf18497b2f2749a
voditelkatka <voditelkatka@gmail.com>
2021-03-04 10:22:09 +0300

    add: converting a two-dimensional array to one-dimensional

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let b = [];
+for (let i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+        b.push(matrix[i][j]);
+    }
+}
+  return b;
 }
